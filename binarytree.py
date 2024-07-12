2c91cd61d975828dc0d99d4aa9c160e3792cda72
janesh-kiddev <officialjanesh12@gmail.com>
2024-07-12 10:34:00 +0530

    commit for binarytree.py

--- binarytree.py
+++ binarytree.py
@@ -51,3 +51,33 @@ postorder(root)
 
 print("\nPreorder traversal")
 preorder(root)
+
+#2
+
+class Tree_nodes:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def search(root,target):
+    if root is None:
+        return None
+    
+    if root.data == target:
+        return root.data
+    
+    if target < root.data:
+        return search(root.left,target)
+    else:
+        return search(root.right,target)
+
+
+root = Tree_nodes(23)
+root.left = Tree_nodes(17)
+root.right = Tree_nodes(56)
+root.left.left = Tree_nodes(8)
+root.left.right = Tree_nodes(89)
+
+print(f'\n{search(root,17)}')
+

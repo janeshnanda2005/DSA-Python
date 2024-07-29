eb60e2cedc31f88cdceb8a924fa6dc607dae865f
janesh-kiddev <officialjanesh12@gmail.com>
2024-07-29 19:43:16 +0530

    commit changes

---
+++ graph/graph.py
@@ -0,0 +1,26 @@
+def dfs_without_library(graph, start):
+
+  visited = set()
+  stack = [start]
+
+  while stack:
+    vertex = stack.pop()
+    if vertex not in visited:
+      visited.add(vertex)
+      print(vertex, end=' ') 
+      for neighbor in graph[vertex]:
+        stack.append(neighbor)
+
+  return visited
+
+
+graph = {
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': ['F'],
+  'F': []
+}
+
+dfs_without_library(graph, 'A')

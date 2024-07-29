b5eb3cd5b053c663d7212fd6c144c5c619502590
janesh-kiddev <officialjanesh12@gmail.com>
2024-07-29 20:49:58 +0530

    commit changes

---
+++ pathfinding/dijikistra.py
@@ -0,0 +1,25 @@
+import heapq
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        if current_distance > distances[current_node]:
+            continue
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    return distances
+
+graph = {
+    'A': {'B': 5, 'C': 3},
+    'B': {'D': 2},
+    'C': {'B': 1, 'D': 6},
+    'D': {'A': 1}
+}
+start_node = 'A'
+shortest_distances = dijkstra(graph, start_node)
+print(shortest_distances)
